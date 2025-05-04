391725e52aec500738185e55a78d3522b6f52486
Hardik Modha <modha.hardik27@gmail.com>
2025-05-04 15:27:07 +0530

    Added Email authentioncation

--- Sources/SupabaseManager/SupabaseManager.swift
+++ Sources/SupabaseManager/SupabaseManager.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  SupabaseManager.swift
+//  SupabaseManager
+//
+//  Created by Hardik Modha on 04/05/25.
+//
+
+import Foundation
+import Supabase
+
+public actor SupabaseManager {
+    
+    public static let shared = SupabaseManager()
+    
+    //MARK: - Private Properties
+    private var client: SupabaseClient?
+    
+    public static func configure(withURL supabaseURL: URL, andKey supabaseKey: String) async {
+        await shared.setup(withURL: supabaseURL, andKey: supabaseKey)
+    }
+    
+    private func setup(withURL supabaseURL: URL, andKey supabaseKey: String) async {
+        self.client = SupabaseClient(supabaseURL: supabaseURL, supabaseKey: supabaseKey)
+    }
+    
+    public func getClient() -> SupabaseClient? {
+        guard let client = self.client else { return nil }
+        return client
+    }
+    
+}
